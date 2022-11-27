2a841bc253da68b5761cba15ffb0a5cc2f8cf88c
Adham <adhamnotheare@gmail.com>
2022-11-27 12:53:50 +0530

    start 2

--- git.cpp
+++ git.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	cout << "HELLO" << endl;
+	cout << "HELLO 2" << endl;
 	cout << "HELLO 3" << endl;
-	cout << "HELLO 6" << endl;
+	cout << "HELLO 4" << endl;
 	cout << "HELLO 5" << endl;
-	cout << "HELLO 8" << endl;
 	return 0;
 }

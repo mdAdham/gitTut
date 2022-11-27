d617fcaa1c6aa69756c8d323dc4a5f642f0485de
Adham <adhamnotheare@gmail.com>
2022-11-27 12:56:10 +0530

    NOPE

--- git.cpp
+++ git.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	cout << "HELLO" << endl;
+	cout << "HELLO 1" << endl;
 	cout << "HELLO 2" << endl;
 	cout << "HELLO 3" << endl;
 	cout << "HELLO 4" << endl;
-	cout << "HELLO 5" << endl;
 	return 0;
 }
